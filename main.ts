81bd1321c2b32c93e111615ea8dae98fc717b6ac
FGtatsuro <204491+FGtatsuro@users.noreply.github.com>
2022-07-19 02:16:15 +0900

    Use the embedded environment(PWDEBUG) to run scripts in headed mode.

--- main.ts
+++ main.ts
@@ -3,17 +3,13 @@ import { setTimeout } from 'timers/promises'
 import { chromium } from '@playwright/test'
 
 (async () => {
-  const options = {
-    headless: true
-  }
-
-  if (process.env['HEADLESS'] === 'false') {
-    options['headless'] = false
-  }
-
-  const browser = await chromium.launch(options)
+  const browser = await chromium.launch()
   const page = await browser.newPage()
   await page.goto('https://example.com')
-  await setTimeout(5000)
+
+  // Ref: https://playwright.dev/docs/debug#run-in-debug-mode
+  if (process.env['PWDEBUG'] === 'console') {
+    await setTimeout(5000)
+  }
   await browser.close();
 })();
